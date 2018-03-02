5d7a13867b0a42f1d9747215ca6ad782988beded
Jordi Farré <jordi.farre@mango.com>
2018-03-02 14:32:59 +0100

    verse with zero bottles implemented

--- java/src/main/java/BeerSong.java
+++ java/src/main/java/BeerSong.java
@@ -1,6 +1,10 @@
 public class BeerSong {
 
     public String sing(int bottles, int verse) {
+        if (bottles == 0) {
+            return "No more bottles of beer on the wall, no more bottles of beer.\n" +
+                    "Go to the store and buy some more, 99 bottles of beer on the wall.\n\n";
+        }
         if (bottles == 1) {
             return "1 bottle of beer on the wall, 1 bottle of beer.\n" +
                     "Take it down and pass it around, no more bottles of beer on the wall.\n\n";
