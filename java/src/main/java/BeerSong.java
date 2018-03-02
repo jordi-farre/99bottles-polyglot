b85999443d919cda185ef8d02e79d9bae28c2551
Jordi Farré <jordi.farre@mango.com>
2018-03-02 14:25:46 +0100

    single last generic verse implementation

--- java/src/main/java/BeerSong.java
+++ java/src/main/java/BeerSong.java
@@ -1,6 +1,10 @@
 public class BeerSong {
 
     public String sing(int bottles, int verse) {
+        if (bottles == 3) {
+            return "3 bottles of beer on the wall, 3 bottles of beer.\n" +
+                    "Take one down and pass it around, 2 bottles of beer on the wall.\n\n";
+        }
         return "99 bottles of beer on the wall, 99 bottles of beer.\n" +
                 "Take one down and pass it around, 98 bottles of beer on the wall.\n\n";
     }
