f7a268bbddca064d9c5b81668fbfbb4f3ee4cd9a
Jordi Farré <jordi.farre@mango.com>
2018-03-02 14:23:54 +0100

    sing first verse implementation

--- java/src/main/java/BeerSong.java
+++ java/src/main/java/BeerSong.java
@@ -1,7 +1,8 @@
 public class BeerSong {
 
     public String sing(int bottles, int verse) {
-        throw new UnsupportedOperationException();
+        return "99 bottles of beer on the wall, 99 bottles of beer.\n" +
+                "Take one down and pass it around, 98 bottles of beer on the wall.\n\n";
     }
 
     public String singSong() {
